c1978b3fe83eb1a12dba36558d534c733dbaec29
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-10-26 16:20:19 +0200

    Prima base e strategia di calcolo elaborati

---
+++ main.js
@@ -0,0 +1,16 @@
+const distance = parseInt(prompt("Inserire distanza da percorrere"));
+const age = parseInt(prompt("Inserire età"));
+const kilometerP = 0.21 * distance;
+
+
+
+if (age <= 18) {
+    const priceyoung = kilometerP - ((20/100)*kilometerP)
+
+    
+}
+
+else (age > 18, <= 65) {
+    const priceold = kilometerP - ((40/100)*kilometerP)
+    
+}
