7abc55c174cb78cdaa36c53a75ec90f4c0aca151
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-10-26 17:13:54 +0200

    problema sul rigo 20 del js

--- main.js
+++ main.js
@@ -6,11 +6,17 @@ const kilometerP = 0.21 * distance;
 
 if (age <= 18) {
     const priceyoung = kilometerP - ((20/100)*kilometerP)
+    document.getElementById("prezzogiovani").innerHTML = `Il prezzo del biglietto è di : ${priceyoung} euro`
 
     
 }
 
-else (age >= 65) {
+else if (age >= 65) {
     const priceold = kilometerP - ((40/100)*kilometerP)
+    document.getElementById("prezzoanziani").innerHTML = `Il prezzo del biglietto è di : ${priceold} euro`
     
+}
+else if (age >18, age <65) {
+    document.getElementById("prezzonormale").innerHTML = `Il prezzo del biglietto è di : ${kilometerP} euro`
+
 }
