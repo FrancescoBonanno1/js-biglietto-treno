eebe4a9b389ec2cb9bd717d3905604579cba6436
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-10-26 17:32:03 +0200

    versione logicamente corretta e sintassi appropriata del ciclo

--- main.js
+++ main.js
@@ -5,18 +5,18 @@ const kilometerP = 0.21 * distance;
 
 
 if (age <= 18) {
-    const priceyoung = kilometerP - ((20/100)*kilometerP)
-    document.getElementById("prezzogiovani").innerHTML = `Il prezzo del biglietto è di : ${priceyoung} euro`
+    const priceyoung = kilometerP - ((20/100)*kilometerP);
+    document.getElementById("prezzogiovani").innerHTML = `Il prezzo del biglietto è di : ${priceyoung} euro`;
 
     
 }
 
 else if (age >= 65) {
-    const priceold = kilometerP - ((40/100)*kilometerP)
-    document.getElementById("prezzoanziani").innerHTML = `Il prezzo del biglietto è di : ${priceold} euro`
+    const priceold = kilometerP - ((40/100)*kilometerP);
+    document.getElementById("prezzoanziani").innerHTML = `Il prezzo del biglietto è di : ${priceold} euro`;
     
 }
-else if (age >18, age <65) {
-    document.getElementById("prezzonormale").innerHTML = `Il prezzo del biglietto è di : ${kilometerP} euro`
+else {
+    document.getElementById("prezzonormale").innerHTML = `Il prezzo del biglietto è di : ${kilometerP} euro`;
 
 }
