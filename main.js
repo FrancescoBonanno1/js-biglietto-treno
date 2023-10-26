0720e5d79a8022f0838964f10f0454abcd96ab9c
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-10-26 17:36:24 +0200

    versione finale con prezzo a due cifre decimali

--- main.js
+++ main.js
@@ -4,19 +4,20 @@ const kilometerP = 0.21 * distance;
 
 
 
+
 if (age <= 18) {
     const priceyoung = kilometerP - ((20/100)*kilometerP);
-    document.getElementById("prezzogiovani").innerHTML = `Il prezzo del biglietto è di : ${priceyoung} euro`;
+    document.getElementById("prezzogiovani").innerHTML = `Il prezzo del biglietto è di : ${priceyoung.toPrecision(4)} euro`;
 
     
 }
 
 else if (age >= 65) {
     const priceold = kilometerP - ((40/100)*kilometerP);
-    document.getElementById("prezzoanziani").innerHTML = `Il prezzo del biglietto è di : ${priceold} euro`;
+    document.getElementById("prezzoanziani").innerHTML = `Il prezzo del biglietto è di : ${priceold.toPrecision(4)} euro`;
     
 }
 else {
-    document.getElementById("prezzonormale").innerHTML = `Il prezzo del biglietto è di : ${kilometerP} euro`;
+    document.getElementById("prezzonormale").innerHTML = `Il prezzo del biglietto è di : ${kilometerP.toPrecision(4)} euro`;
 
 }
